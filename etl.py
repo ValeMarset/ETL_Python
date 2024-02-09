27bb580e87283310c205de1e96e5915ef905aea7
Valentina Marset <valemarset05@gmail.com>
2024-02-09 09:17:25 -0300

    Creación del método transformation_data

--- etl.py
+++ etl.py
@@ -5,3 +5,21 @@ def extraction_data_from_api(url_api):
   r = requests.get(url_api)
   data = r.json()
   return data
+
+
+def transformation_data(data):
+  data_harry_potter = []
+
+  for data_character in data:
+    data_character = {
+      'actor': data_character['actor'],
+      'nombres_alternativos': data_character['alternate_names'],
+      'cumpleaños': data_character['dateOfBirth'],
+      'genero': data_character['gender'],
+      'casa': data_character['house'],
+      'nombre': data_character['name']
+    }
+    data_harry_potter.append(data_character)
+
+  return data_harry_potter
+
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import etl
+import pprint as p
 
 if __name__ == '__main__':
   result = etl.extraction_data_from_api('https://hp-api.onrender.com/api/characters')
-  print(result)
+  transformed_data = etl.transformation_data(result)
